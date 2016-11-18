7158893a2736ba9e7460756a984b6b7cf43ad8af
Jenkyrados <quentin.dauchy@free.fr>
2016-11-19 00:34:33 +0100

    Coinsum problems 76 and 77

--- Ruby/prob77.rb
+++ Ruby/prob77.rb
@@ -1,15 +1,22 @@
+require 'prime'
 $hash = Hash.new(0)
 def recsums(n,maxsize)
   if n < 0 then return 0 end
-  if maxsize == 1 then return 1 end
   if n == 0 then return 1 end
   if $hash[[n,maxsize]] != 0 then return $hash[[n,maxsize]] end
   if maxsize == 0 then return 0 end
-  (1..maxsize).reverse_each do |a|
+  Prime.each do |a|
+    if a > maxsize then break end
     $hash[[n,maxsize]] += recsums(n-a,a)
   end
   return $hash[[n,maxsize]]
 end
 
-recsums(100,99)
-puts $hash[[100,99]]
+x = 0
+i = 2
+while x < 5000
+  x = recsums(i,i-1)
+  i+=1
+end
+puts i-1
+

fbe677af659894e2f23482d4679d1dd460574313
Jenkyrados <quentin.dauchy@free.fr>
2016-11-19 22:10:57 +0100

    Roots fast calculator (requires BigDecimal) (80)

---
+++ Ruby/prob80.rb
@@ -0,0 +1,17 @@
+require './utils'
+require 'bigdecimal'
+sum = 0
+(2..100).each do |s|
+  if s.issquare? then next end
+  two = BigDecimal.new(2,1000)
+  sbig = BigDecimal.new(s,1000)
+  x = BigDecimal.new(4*(10**(s.to_s.length/2)),1000)
+  tmp = BigDecimal.new(0,1000)
+  while x.round(100) != tmp.round(100)
+    x,tmp = (x + sbig / x) / two,x
+  end
+
+  sum += x.truncate(99).to_s('F').chars.map{|a| a.to_i}.reduce(:+)
+end
+
+puts sum
